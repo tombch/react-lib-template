313c58440787d7f0e6f04e171e31afaa20d5e301
tombch <t.brier@outlook.com>
2025-10-31 14:52:24 +0000

    Removing unused handlers, and default tanstack-query setup

--- lib/App.tsx
+++ lib/App.tsx
@@ -1,25 +1,11 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./app.css";
 
 function Main() {
   return <div>Hello World!</div>;
 }
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-      refetchOnMount: false,
-    },
-  },
-});
-
 function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Main />
-    </QueryClientProvider>
-  );
+  return <Main />;
 }
 
 export default App;
